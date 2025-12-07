4e39ca5a624c0d9108f570e6f04b1c54d68a46cb
Kerem Batın Topal <kermbatn@gmail.com>
2025-12-07 10:01:11 +0300

    Single comment, Multiple comment

---
+++ src/main/java/com/kerembatintopal/_2_DataTypes.java
@@ -0,0 +1,28 @@
+package com.kerembatintopal;
+
+public class _2_DataTypes {
+    /*
+     Türkçe karakter kullanmayız; üğişçö, ÜĞİŞÇÖ
+     */
+    static void main() {
+        // Primitive Types
+        // Tam Sayılar
+        byte primitiveByte=127;
+        short primiteShort=140;
+        int primitiveShort=140;
+
+        //Floating Point
+        float primitiveFloat=14.53f;
+        double primitiveDouble=141414.5353535353;
+
+        //other
+        boolean primitiveBoolean=true;
+        char primitiveChar='@';
+        System.out.println("primitiveChar");
+
+
+
+
+
+    }//end psvm
+}//end class _2_DataTypes
---
+++ src/main/java/com/kerembatintopal/_3_WrapperTypes.java
@@ -0,0 +1,31 @@
+package com.kerembatintopal;
+
+public class _3_WrapperTypes {
+    /*
+
+    Wrapper types:
+    OOP (Nesne Yönelimli programalama) daha yakındır.
+    daha yavaş
+     heap memory kullanılır ,
+     daha yavaştır Stack memory'e göre
+     null verebilirsiniz
+    *
+    *  */
+    static void main() {
+        // Primitive Types
+        // Tam Sayılar
+        Byte wrapperByte = 127;
+        Short primiteShort = 140;
+        Integer wrapperShort = 140;
+        Long wrapperLong = 1515151114145L;
+
+        //Floating Point
+        Float wrapperFloat = 14.53f;
+        Double wrapperDouble = 141414.5353535353;
+
+        //other
+        Boolean wrapperBoolean = true;
+        Character wrapperChar = '@';
+        System.out.println("wrapperChar");
+    }
+}
